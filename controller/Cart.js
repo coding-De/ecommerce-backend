84ce1a6e06aed9a1d68a55b981b80df06bac484e
omkar <omkarramtripathi@gmail.com>
2024-02-18 09:00:16 +0530

    comiite

---
+++ controller/Cart.js
@@ -0,0 +1,50 @@
+const { Cart } = require('../model/Cart');
+
+exports.fetchCartByUser = async (req, res) => {
+  console.log(req)
+  // const { id } = req.user;
+  try {
+    const cartItems = await Cart.find();
+
+    res.status(200).json(cartItems);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.addToCart = async (req, res) => {
+  const id = req.body.user;
+  const cartItem = [req.body];
+  const cart = new Cart(req.body);
+  try {
+    const doc = await cart.save();
+    const result = await doc.populate('product');
+    res.status(201).json(result);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.deleteFromCart = async (req, res) => {
+    const { id } = req.params;
+    try {
+    const doc = await Cart.findByIdAndDelete(id);
+    res.status(200).json(doc);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+exports.updateCart = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const cart = await Cart.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    // const result = await cart.populate('cart');
+
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
---
+++ model/Cart.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+
+const cartSchema = new Schema(
+//     {
+//     item: {
+//         quantity: { type: Number, required: true },
+//         product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+//     },
+//     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+//     size: { type : Schema.Types.Mixed},
+//     color: { type : Schema.Types.Mixed},
+// }
+
+ {
+        title: { type : Schema.Types.Mixed},
+        description: { type : Schema.Types.Mixed},
+        price: { type: Number },
+        discountPercentage: { type: Number },
+        rating: { type: Number },
+        stock: { type: Number },
+        brand: { type : Schema.Types.Mixed},
+        category: { type : Schema.Types.Mixed},
+        thumbnail: { type : Schema.Types.Mixed},
+        images:{ type : Schema.Types.Mixed},
+        productId: { type: Number },
+        quantity: { type: Number },
+        user: { type: String },
+        size: { type : Schema.Types.Mixed},
+        color: { type : Schema.Types.Mixed},
+     }
+)
+const virtual  = cartSchema.virtual('id');
+virtual.get(function(){
+    return this._id;
+})
+cartSchema.set('toJSON',{
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc,ret) { delete ret._id}
+})
+
+
+exports.Cart = mongoose.model('Cart',cartSchema)
